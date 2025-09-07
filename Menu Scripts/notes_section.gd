d89c2af71a2fc23e045df6709ce296e5c54f3cc8
JustinPalinsad <justinpalinsad02@gmail.com>
2025-09-07 11:24:26 +0800

    Update notes_section.gd

--- Menu Scripts/notes_section.gd
+++ Menu Scripts/notes_section.gd
@@ -9,50 +9,60 @@ func _on_back_pressed() -> void:
 
 
 func _on_lesson_1_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
 
 
 func _on_lesson_2_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
 
 
 func _on_lesson_3_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
 
 
 func _on_lesson_4_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
 
 
 func _on_lesson_5_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
 
 
 func _on_lesson_6_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
 
 
 func _on_lesson_7_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
 
 
 func _on_lesson_8_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
 
 
 func _on_lesson_9_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
 
 
 func _on_lesson_10_pressed() -> void:
+	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
