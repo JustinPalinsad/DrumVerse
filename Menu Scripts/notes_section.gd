382be13c2edcea4f43d13a6ca9ebe52a1a440a3e
JustinPalinsad <justinpalinsad02@gmail.com>
2025-09-07 11:21:33 +0800

    added assets
    new highlight for lesson cards

--- Menu Scripts/notes_section.gd
+++ Menu Scripts/notes_section.gd
@@ -6,3 +6,53 @@ func _on_back_pressed() -> void:
 	$BackSoundPlayer.play()
 	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Menu Scenes/main_menu.tscn")
+
+
+func _on_lesson_1_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_lesson_2_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_lesson_3_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_lesson_4_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_lesson_5_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_lesson_6_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_lesson_7_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_lesson_8_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_lesson_9_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_lesson_10_pressed() -> void:
+	await get_tree().create_timer(0.2).timeout
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
