a850ed1be18d40b9aba99dbad07f2b04b71d634b
Kh4yser <khayriymera@gmail.com>
2025-09-13 18:06:20 +0800

    Added new stuff
    New Music Assets
    
    New Metronome
    
    Timing has to be adjusted
    
    New Hitline

--- Scenes/metronomev_2.gd
+++ Scenes/metronomev_2.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+var bpmx = 60
 
-func _ready() -> void:
+var bpm = bpmx/60.0
+
+func _ready():
+	$AnimationPlayer.speed_scale = bpm
 	$AnimationPlayer.play("swing")
+	$BPM.text = str(bpmx)
