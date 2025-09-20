c9634122f5360da17b3aa5c36239b7fa513dbdfb
JustinPalinsad <justinpalinsad02@gmail.com>
2025-09-20 15:04:42 +0800

    finished notes tres content
    no icon yet

--- Menu Scripts/notes_section.gd
+++ Menu Scripts/notes_section.gd
@@ -17,3 +17,44 @@ func _on_notes_3_pressed() -> void:
 	var loaded_resource = load("res://NotesResource/quarternote.tres")
 	GameState.selected_notes_resource = loaded_resource
 	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_notes_4_pressed() -> void:
+	var loaded_resource = load("res://NotesResource/eightnote.tres")
+	GameState.selected_notes_resource = loaded_resource
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_notes_5_pressed() -> void:
+	var loaded_resource = load("res://NotesResource/sixteenthnote.tres")
+	GameState.selected_notes_resource = loaded_resource
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_notes_6_pressed() -> void:
+	var loaded_resource = load("res://NotesResource/triplet.tres")
+	GameState.selected_notes_resource = loaded_resource
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_notes_7_pressed() -> void:
+	var loaded_resource = load("res://NotesResource/singlestroke.tres")
+	GameState.selected_notes_resource = loaded_resource
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_notes_8_pressed() -> void:
+	var loaded_resource = load("res://NotesResource/doublestroke.tres")
+	GameState.selected_notes_resource = loaded_resource
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+func _on_notes_9_pressed() -> void:
+	var loaded_resource = load("res://NotesResource/paradiddle.tres")
+	GameState.selected_notes_resource = loaded_resource
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
+
+
+func _on_notes_10_pressed() -> void:
+	var loaded_resource = load("res://NotesResource/flam.tres")
+	GameState.selected_notes_resource = loaded_resource
+	get_tree().change_scene_to_file("res://Menu Scenes/notes_container.tscn")
