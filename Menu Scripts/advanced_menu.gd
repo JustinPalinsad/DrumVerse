ca8417bae0faa9d7154970d57023adb8bc87e811
yunglodi <markjohnjayme@gmail.com>
2025-09-25 01:17:49 +0800

    Lesson19-23

--- Menu Scripts/advanced_menu.gd
+++ Menu Scripts/advanced_menu.gd
@@ -166,43 +166,115 @@ func _on_advance_lesson_18_pressed() -> void:
 		push_error("Failed to load module or learning module!")
 
 
-#func _on_advance_lesson_19_pressed() -> void:
-	#GameState.lessons = 19
-	#$ClickSoundPlayer.play()
-	#await get_tree().create_timer(0.2).timeout
-	#
-	#print("Loading Module19.tres and Module19_Learning.tres...")
-#
-	#var module_resource = load("res://Module/Lesson19/Module19.tres")
-	#var learning_resource = load("res://Module/Lesson19/Module19_Learning.tres")
-	#
-	#if module_resource and learning_resource:
-		#print("Both module and learning module loaded.")
-		#GameState.selected_module = module_resource
-		#GameState.selected_learning_module = learning_resource
-		#call_deferred("_goto_sample_scene")
-	#else:
-		#push_error("Failed to load module or learning module!")
-#
-#
-#func _on_advance_lesson_20_pressed() -> void:
-	#GameState.lessons = 20
-	#$ClickSoundPlayer.play()
-	#await get_tree().create_timer(0.2).timeout
-	#
-	#print("Loading Module20.tres and Module20_Learning.tres...")
-#
-	#var module_resource = load("res://Module/Lesson20/Module20.tres")
-	#var learning_resource = load("res://Module/Lesson20/Module20_Learning.tres")
-	#
-	#if module_resource and learning_resource:
-		#print("Both module and learning module loaded.")
-		#GameState.selected_module = module_resource
-		#GameState.selected_learning_module = learning_resource
-		#call_deferred("_goto_sample_scene")
-	#else:
-		#push_error("Failed to load module or learning module!")
+func _on_advance_lesson_19_pressed() -> void:
+	GameState.lessons = 19
+	$ClickSoundPlayer.play()
+	await get_tree().create_timer(0.2).timeout
+	
+	print("Loading Module19.tres and Module19_Learning.tres...")
+
+	var module_resource = load("res://Module/Lesson19/Module19.tres")
+	var learning_resource = load("res://Module/Lesson19/Module19_Learning.tres")
+	
+	if module_resource and learning_resource:
+		print("Both module and learning module loaded.")
+		GameState.selected_module = module_resource
+		GameState.selected_learning_module = learning_resource
+		call_deferred("_goto_sample_scene")
+	else:
+		push_error("Failed to load module or learning module!")
+
+
+func _on_advance_lesson_20_pressed() -> void:
+	GameState.lessons = 20
+	$ClickSoundPlayer.play()
+	await get_tree().create_timer(0.2).timeout
+	
+	print("Loading Module20.tres and Module20_Learning.tres...")
+
+	var module_resource = load("res://Module/Lesson20/Module20.tres")
+	var learning_resource = load("res://Module/Lesson20/Module20_Learning.tres")
+	
+	if module_resource and learning_resource:
+		print("Both module and learning module loaded.")
+		GameState.selected_module = module_resource
+		GameState.selected_learning_module = learning_resource
+		call_deferred("_goto_sample_scene")
+	else:
+		push_error("Failed to load module or learning module!")
+
+func _on_advance_lesson_21_pressed() -> void:
+	GameState.lessons = 21
+	$ClickSoundPlayer.play()
+	await get_tree().create_timer(0.2).timeout
+	
+	print("Loading Module21.tres and Module21_Learning.tres...")
+
+	var module_resource = load("res://Module/Lesson21/Module21.tres")
+	var learning_resource = load("res://Module/Lesson21/Module21_Learning.tres")
+	
+	if module_resource and learning_resource:
+		print("Both module and learning module loaded.")
+		GameState.selected_module = module_resource
+		GameState.selected_learning_module = learning_resource
+		call_deferred("_goto_sample_scene")
+	else:
+		push_error("Failed to load module or learning module!")
 		
+func _on_advance_lesson_22_pressed() -> void:
+	GameState.lessons = 22
+	$ClickSoundPlayer.play()
+	await get_tree().create_timer(0.2).timeout
+	
+	print("Loading Module22.tres and Module22_Learning.tres...")
+
+	var module_resource = load("res://Module/Lesson22/Module22.tres")
+	var learning_resource = load("res://Module/Lesson22/Module22_Learning.tres")
+	
+	if module_resource and learning_resource:
+		print("Both module and learning module loaded.")
+		GameState.selected_module = module_resource
+		GameState.selected_learning_module = learning_resource
+		call_deferred("_goto_sample_scene")
+	else:
+		push_error("Failed to load module or learning module!")
+		
+func _on_advance_lesson_23_pressed() -> void:
+	GameState.lessons = 23
+	$ClickSoundPlayer.play()
+	await get_tree().create_timer(0.2).timeout
+	
+	print("Loading Module23.tres and Module23_Learning.tres...")
+
+	var module_resource = load("res://Module/Lesson23/Module23.tres")
+	var learning_resource = load("res://Module/Lesson23/Module23_Learning.tres")
+	
+	if module_resource and learning_resource:
+		print("Both module and learning module loaded.")
+		GameState.selected_module = module_resource
+		GameState.selected_learning_module = learning_resource
+		call_deferred("_goto_sample_scene")
+	else:
+		push_error("Failed to load module or learning module!")
+		
+func _on_advance_lesson_24_pressed() -> void:
+	GameState.lessons = 24
+	$ClickSoundPlayer.play()
+	await get_tree().create_timer(0.2).timeout
+	
+	print("Loading Module24.tres and Module24_Learning.tres...")
+
+	var module_resource = load("res://Module/Lesson24/Module24.tres")
+	var learning_resource = load("res://Module/Lesson24/Module24_Learning.tres")
+	
+	if module_resource and learning_resource:
+		print("Both module and learning module loaded.")
+		GameState.selected_module = module_resource
+		GameState.selected_learning_module = learning_resource
+		call_deferred("_goto_sample_scene")
+	else:
+		push_error("Failed to load module or learning module!")
+
 func _goto_sample_scene() -> void:
 	var sample_scene = load("res://Sample/sample_scene.tscn").instantiate()
 	get_tree().current_scene.queue_free()
