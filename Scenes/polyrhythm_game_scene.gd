e186ee1c35ed87d6f06f6efd74eb04631f8dd6f0
JustinPalinsad <justinpalinsad02@gmail.com>
2025-09-28 10:15:37 +0800

    changed animation on start up

--- Scenes/polyrhythm_game_scene.gd
+++ Scenes/polyrhythm_game_scene.gd
@@ -2,5 +2,7 @@ extends Node2D
 
 
 func _ready() -> void:
-	$Middle_Point/HitLineTop/MovingCircleTop/TopBallAnim.play("Top_Line_2")
-	$Middle_Point/HitLineBottom/MovingCircleBottom/BottomBallAnim.play("Bottom_Line_2")
+	#$Middle_Point/HitLineTop/MovingCircleTop/TopBallAnim.play("Top_Line_2")
+	#$Middle_Point/HitLineBottom/MovingCircleBottom/BottomBallAnim.play("Bottom_Line_2")
+	$"Moving Circe/MovingCircleTop/TopBallAnim".play("Top_Line")
+	$"Moving Circe/MovingCircleBottom/BottomBallAnim".play("Bottom_Line")
