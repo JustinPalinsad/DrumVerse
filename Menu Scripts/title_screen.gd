395d328f42b0feee27aec5be0640308c6d6f82d0
JustinPalinsad <justinpalinsad02@gmail.com>
2025-10-18 12:55:53 +0800

    fixed title and main menu

--- Menu Scripts/title_screen.gd
+++ Menu Scripts/title_screen.gd
@@ -4,10 +4,17 @@ extends Control
 @onready var text_animPlayer = $"Tap Anywhere/Text Animation" #for text
 @onready var drum_animPlayer = $"Control/Drum/Drum Animation" #for drum
 
+
 func _ready() -> void:
 	text_animPlayer.play("text_blink_animation")
 	
 func _on_start_button_pressed() -> void:
+	# Disable input by hiding the button or setting its 'visible' to false
+
+	$"Control2/Start Button".visible = false
+	# alternatively, you can block input with:
+	# start_button.set_process_input(false)
+	
 	text_animPlayer.play("text_blink_faster_animation")
 	drum_animPlayer.play("drum_bounce_animation")
 	$ClickSoundPlayer.play()
@@ -16,4 +23,3 @@ func _on_start_button_pressed() -> void:
 	print("Going to Main Menu...!")
 	await text_animPlayer.animation_finished
 	get_tree().change_scene_to_file("res://Menu Scenes/main_menu.tscn")
-	
